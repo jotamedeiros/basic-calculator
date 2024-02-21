b8796dbacd2a09f32c39316728243aef36a54e28
João Guilherme Medeiros <devjoaogui@gmail.com>
2024-02-21 18:30:44 -0300

    add evento p/ teclas numéricas e decimal

---
+++ script.js
@@ -0,0 +1,31 @@
+const teclasNum = [...document.querySelectorAll('.num')]
+const teclasOp = [...document.querySelectorAll('.op')]
+const teclaRes = document.querySelector('.res')
+const teclaCopy = document.querySelector('#tCpy')
+const teclaLimpar = document.querySelector('#tLimpar')
+const display = document.querySelector('#display')
+
+let sinal = false
+let decimal = false
+
+
+teclasNum.map((el, i) => {
+    el.addEventListener('click', (evt) => {
+        sinal = false
+        if (evt.target.innerHTML == '.') {
+            if (!decimal) {
+                decimal = true
+                if (display.innerHTML == '0') {
+                    display.innerHTML = '0.'
+                } else {
+                    display.innerHTML += evt.target.innerHTML
+                }
+            }
+        } else {
+            if (display.innerHTML == '0') {
+                display.innerHTML = ''
+            }
+            display.innerHTML += evt.target.innerHTML
+        }
+    })
+})
